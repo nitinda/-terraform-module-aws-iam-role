7502d0bcf6830cae76ed883f8dc0aeead3259860
nitin-das <nitin.das@thomson.co.uk>
2020-04-15 13:22:16 +0100

    Modified Code

--- iam-role.tf
+++ iam-role.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_role" "iam_role" {
-  name                   = var.name
-  name_prefix            = var.name_prefix
-  assume_role_policy     = var.assume_role_policy
-  force_detach_policies  = var.force_detach_policies
-  path                   = var.path
-  description            = var.description
-  permissions_boundary   = var.permissions_boundary
-  tags                   = var.tags
+  name                  = var.name
+  name_prefix           = var.name_prefix
+  assume_role_policy    = var.assume_role_policy
+  force_detach_policies = var.force_detach_policies
+  path                  = var.path
+  description           = var.description
+  permissions_boundary  = var.permissions_boundary
+  tags                  = var.tags
 
   lifecycle {
     create_before_destroy = true
